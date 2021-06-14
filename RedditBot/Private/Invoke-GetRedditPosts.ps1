73d5bfe52a1946dddbce65fc830587606bc313fb
zach.fegan <zachfegan@gmail.com>
2021-06-14 18:04:32 -0500

    Cleaned out project

--- RedditBot/Private/Invoke-GetRedditPosts.ps1
+++
@@ -1,6 +0,0 @@
-function Invoke-GetRedditPosts {
-    param (
-        $Subname
-    )
-    return (Invoke-Restmethod -Method Get -Uri https://www.reddit.com/r/$Subname/new.json).data.children.data
-}
